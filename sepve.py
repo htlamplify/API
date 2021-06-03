1a087a536877e6abcc89dd3a6b91808cb0d968e3
Philipp Lackinger <Phillackinger@gmx.at>
2021-06-03 13:19:43 +0200

    :tada: Initial Commit

--- sepve.py
+++ sepve.py
@@ -17,32 +17,32 @@ class SEPVE:
         self.i2c_addr = addr
         self.bus = SMBus(1)
 
-    async def setInputChannelFunction(self, channel: int):
+    def setInputChannelFunction(self, channel: int):
         self.bus.write_byte_data(self.i2c_addr, INPUT_SELECTOR.get("SUB_ADDR"), INPUT_SELECTOR.get(channel))
 
-    async def setInputGainFunction(self, gain: int):
+    def setInputGainFunction(self, gain: int):
         self.bus.write_byte_data(self.i2c_addr, INPUT_GAIN.get("SUB_ADDR"), INPUT_GAIN.get(gain))
 
-    async def setVolumeFunction(self, volume):
+    def setVolumeFunction(self, volume):
         if volume == "MUTE":
             volume = VOLUME.get("MUTE")
         self.bus.write_byte_data(self.i2c_addr, VOLUME.get("SUB_ADDR"), volume)
 
-    async def setBassGainFunction(self, gain: int):
+    def setBassGainFunction(self, gain: int):
         self.bus.write_byte_data(self.i2c_addr, BASS_GAIN.get("SUB_ADDR"), BASS_GAIN.get(gain))
 
-    async def setMidGainFunction(self, gain: int):
+    def setMidGainFunction(self, gain: int):
         self.bus.write_byte_data(self.i2c_addr, TREBLE_GAIN.get("SUB_ADDR"), TREBLE_GAIN.get(gain))
 
-    async def setTrebleGainFunction(self, gain: int):
+    def setTrebleGainFunction(self, gain: int):
         self.bus.write_byte_data(self.i2c_addr, TREBLE_GAIN.get("SUB_ADDR"), TREBLE_GAIN.get(gain))
 
-    async def setSpeakerAttRFunction(self, att):
+    def setSpeakerAttRFunction(self, att):
         if att == "MUTE":
             att = SPEAKER_ATT_R.get("MUTE")
         self.bus.write_byte_data(self.i2c_addr, SPEAKER_ATT_R.get("SUB_ADDR"), att)
 
-    async def setSpeakerAttLFunction(self, att):
+    def setSpeakerAttLFunction(self, att):
         if att == "MUTE":
             att = SPEAKER_ATT_L.get("MUTE")
         self.bus.write_byte_data(self.i2c_addr, SPEAKER_ATT_L.get("SUB_ADDR"), att)

8cd478557ff054c21e2592d711d9d9112531b864
FlorianRupp02 <florianrupp20@gmail.com>
2022-03-02 16:03:44 +0100

    changed V1 of equalizer/FFT object tp an actual object

--- FFT.py
+++ FFT.py
@@ -1,47 +1,41 @@
- 
- #A sketch that grabs the frequency signal level measured by the Mixed Signal
- #Integration MSGEQ7. This sketch is designed to work with the MSGEQ7 Breakout Board 
- #available at the Rheingold Heavy website.
- 
- #This sketch expects the following pin assignments...
- #STROBE = DIGITAL 7
- #RESET  = DIGITAL 8
- #OUTPUT = ANALOG  5
- 
- #Website:   https://rheingoldheavy.com/msgeq7-arduino-tutorial-01-getting-started
- #Datasheet: http://www.mix-sig.com/images/datasheets/MSGEQ7.pdf
- 
+from smbus2 import SMBus
 import RPI.GPIO as GPIO
 import time 
 
-strobePin  = GPIO.setup(13, GPIO.OUT)   # Strobe Pin on the MSGEQ7
-resetPin   = GPIO.setup(19, GPIO.OUT)   # Reset Pin on the MSGEQ7
-level[7]          # An array to hold the values from the 7 frequency bands
-audioSample = GPIO.setup(2, GPIO.IN) # Output Pin on the MSGEQ7
+class EQ:
+    def __init__(self, strobe, reset, addr, reg):
+        strobePin  = strobe
+        GPIO.setup(strobePin, GPIO.OUT)   # Strobe Pin on the MSGEQ7
+        resetPin   = reset
+        GPIO.setup(resetPin, GPIO.OUT)   # Reset Pin on the MSGEQ7
+        value[7]          # An array to hold the values from the 7 frequency bands
 
-  #// Create an initial state for our pins
-GPIO.output(19, GPIO.LOW)
-GPIO.output(13, GPIO.LOW)
-sleep(0.001)
+        i2c_addr = addr
+        audioSample = SMBus(1).read_byte_data(i2c_addr, reg) # Output of the MSGEQ7
 
- 
-  #// Reset the MSGEQ7 as per the datasheet timing diagram
-GPIO.output(19, GPIO.HIGH)
-sleep(0.001)
-GPIO.output(19, GPIO.LOW)
-GPIO.output(13, GPIO.HIGH)
-sleep(0.001)
+        #// Create an initial state for our pins
+        GPIO.output(resetPin, GPIO.LOW)
+        GPIO.output(strobePin, GPIO.LOW)
+        sleep(0.001)
 
- 
-while true:
-  # Cycle through each frequency band by pulsing the strobe.
-    for i in range(7):
-        GPIO.output(13, GPIO.LOW)
-        sleep(0.0001)                   # Delay necessary due to timing diagram
-        level[i] = audioSample
-        GPIO.output(13, GPIO.HIGH)
-        sleep(0.0001)                   # Delay necessary due to timing diagram  
-    
-    for i in range(7):
-        Value(level[i]) 
         
+        #// Reset the MSGEQ7 as per the datasheet timing diagram
+        GPIO.output(resetPin, GPIO.HIGH)
+        sleep(0.001)
+        GPIO.output(resetPin, GPIO.LOW)
+        GPIO.output(strobePin, GPIO.HIGH)
+        sleep(0.001)
+
+        
+    while true:
+    # Cycle through each frequency band by pulsing the strobe.
+        for i in range(7):
+            GPIO.output(strobePin, GPIO.LOW)
+            sleep(0.0001)                   # Delay necessary due to timing diagram
+            value[i] = audioSample
+            GPIO.output(strobePin, GPIO.HIGH)
+            sleep(0.0001)                   # Delay necessary due to timing diagram  
+        
+        for i in range(7):
+            value[i]
+                

a44a36eb6cb1aab5cca0dd340f473ee128eb9aaf
FlorianRupp02 <florianrupp20@gmail.com>
2022-03-23 16:37:03 +0100

    updated Matrix file

--- ledmatrix.py
+++ ledmatrix.py
@@ -1,5 +1,7 @@
 from FFT import EQ
 import neopixel
+import board
+
 
 class matrix:
     def __init__(self, rows, cols):
@@ -7,23 +9,26 @@ class matrix:
     
     
 class neopixel:
-    def __init__(self, board, rows, cols):
-        self.board = board
+    def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
         self.num = rows*cols
-        pixels = neopixel.NeoPixel(self.board, self.num)
+        pixels = neopixel.NeoPixel(board.D21, self.num)
         for i in range(num):
             pixels[i] = 0x000000
     
     RED = 0x100000
     
     matrix.__init__(self.rows, self.cols)
-    rowwid = rows/8
+    rowwid = rows/7
     
     while true:
         for i in range(rows):
-            for w in range(rowwid):
+            for w in range(rowwid):                 #rowwid = rows per frequencyband
                 for j in range(EQ.value[i]):
-                    pixels[(i*cols)+j] = RED
+                    if(i%2):
+                        pixels[(i*cols)-j] = RED    #every even row is from top to bottom
+                    else:
+                        pixels[(i*cols)+j] = RED    #every odd row is from bottom to top
+
             
